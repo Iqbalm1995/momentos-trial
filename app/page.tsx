5f5e07bfc1ea60951199f0f876a2f52f418ad7db
iqbalm1995 <iqbalm1995@gmail.com>
2024-07-23 19:57:14 +0700

    put everything 1

--- app/page.tsx
+++ app/page.tsx
@@ -82,41 +82,238 @@ export default function Page() {
           >
             <HStack h={"full"} w={"full"} zIndex={0}>
               <Marquee speed={15} direction={"left"}>
-                <Box h={"200px"} mx={1} bg={"#00FFF3"} w={"400px"}></Box>
-                <Box h={"200px"} mx={1} bg={"#00FFF3"} w={"400px"}></Box>
-                <Box h={"200px"} mx={1} bg={"#00FFF3"} w={"400px"}></Box>
-                <Box h={"200px"} mx={1} bg={"#00FFF3"} w={"400px"}></Box>
-                <Box h={"200px"} mx={1} bg={"#00FFF3"} w={"400px"}></Box>
-                {/* <Box h={"200px"} bg={"#00FFF3"} w={"400px"}></Box> */}
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#00FFF3"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#00FFF3"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#00FFF3"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#00FFF3"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c7.staticflickr.com/9/8546/28354329294_bb45ba31fa_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#00FFF3"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c6.staticflickr.com/9/8890/28897154101_a8f55be225_b.jpg)`}
+                ></Box>
               </Marquee>
             </HStack>
             <HStack h={"full"} w={"full"} zIndex={0}>
               <Marquee speed={15} direction={"right"}>
-                <Box h={"400px"} mx={1} bg={"#00D1FF"} w={"280px"}></Box>
-                <Box h={"400px"} mx={1} bg={"#00D1FF"} w={"280px"}></Box>
-                <Box h={"400px"} mx={1} bg={"#00D1FF"} w={"280px"}></Box>
-                <Box h={"400px"} mx={1} bg={"#00D1FF"} w={"280px"}></Box>
-                <Box h={"400px"} mx={1} bg={"#00D1FF"} w={"280px"}></Box>
-                <Box h={"400px"} mx={1} bg={"#00D1FF"} w={"280px"}></Box>
-                <Box h={"400px"} mx={1} bg={"#00D1FF"} w={"280px"}></Box>
+                <Box
+                  h={"400px"}
+                  mx={1}
+                  bg={"#00D1FF"}
+                  w={"280px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c6.staticflickr.com/9/8890/28897154101_a8f55be225_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"400px"}
+                  mx={1}
+                  bg={"#00D1FF"}
+                  w={"280px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c5.staticflickr.com/9/8768/28941110956_b05ab588c1_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"400px"}
+                  mx={1}
+                  bg={"#00D1FF"}
+                  w={"280px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"400px"}
+                  mx={1}
+                  bg={"#00D1FF"}
+                  w={"280px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"400px"}
+                  mx={1}
+                  bg={"#00D1FF"}
+                  w={"280px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"400px"}
+                  mx={1}
+                  bg={"#00D1FF"}
+                  w={"280px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"400px"}
+                  mx={1}
+                  bg={"#00D1FF"}
+                  w={"280px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg)`}
+                ></Box>
               </Marquee>
             </HStack>
             <HStack h={"full"} w={"full"} zIndex={0}>
               <Marquee speed={15} direction={"left"}>
-                <Box h={"200px"} mx={1} bg={"#0087FF"} w={"400px"}></Box>
-                <Box h={"200px"} mx={1} bg={"#0087FF"} w={"400px"}></Box>
-                <Box h={"200px"} mx={1} bg={"#0087FF"} w={"400px"}></Box>
-                <Box h={"200px"} mx={1} bg={"#0087FF"} w={"400px"}></Box>
-                <Box h={"200px"} mx={1} bg={"#0087FF"} w={"400px"}></Box>
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#0087FF"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c7.staticflickr.com/9/8785/28687743710_3580fcb5f0_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#0087FF"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#0087FF"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#0087FF"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c4.staticflickr.com/9/8562/28897228731_ff4447ef5f_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#0087FF"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c2.staticflickr.com/8/7577/28973580825_d8f541ba3f_b.jpg)`}
+                ></Box>
               </Marquee>
             </HStack>
             <HStack h={"full"} w={"full"} zIndex={0}>
               <Marquee speed={15} direction={"right"}>
-                <Box h={"200px"} mx={1} bg={"#0027FF"} w={"400px"}></Box>
-                <Box h={"200px"} mx={1} bg={"#0027FF"} w={"400px"}></Box>
-                <Box h={"200px"} mx={1} bg={"#0027FF"} w={"400px"}></Box>
-                <Box h={"200px"} mx={1} bg={"#0027FF"} w={"400px"}></Box>
-                <Box h={"200px"} mx={1} bg={"#0027FF"} w={"400px"}></Box>
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#0027FF"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c1.staticflickr.com/9/8330/28941240416_71d2a7af8e_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#0027FF"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c1.staticflickr.com/9/8707/28868704912_cba5c6600e_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#0027FF"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c4.staticflickr.com/9/8578/28357117603_97a8233cf5_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#0027FF"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c4.staticflickr.com/8/7476/28973628875_069e938525_b.jpg)`}
+                ></Box>
+                <Box
+                  h={"200px"}
+                  mx={1}
+                  bg={"#0027FF"}
+                  w={"400px"}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="cover"
+                  backgroundImage={`url(https://c6.staticflickr.com/9/8593/28357129133_f04c73bf1e_b.jpg)`}
+                ></Box>
               </Marquee>
             </HStack>
           </VStack>
